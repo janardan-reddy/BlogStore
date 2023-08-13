33f5695cd7c25e187dec0b4979428c27609d05c8
janardan-reddy <janardan6187@gmail.com>
2023-08-13 14:12:04 +0200

    update gitignore
    added __pycache__ to ignore all pycache files while pushing the changes

---
+++ application/routers/blog_router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router= APIRouter(
+    prefix= '/blog',
+    tags= ['blog']
+)
+
+@router.get('')
+def getBlog():
+    return {
+        'data': 'fetching all blogs'
+    }
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from application.routers import blog_router
 
 app = FastAPI()
+app.include_router(blog_router.router)
 
 @app.get('/')
 def index():
